790f0446035f2c5675e5c0de7212cc9bbe47d985
TalonChen <talonc@nycstudents.net>
2026-01-27 14:02:11 -0500

    sadasd

---
+++ app.js
@@ -0,0 +1,20 @@
+const animalFoodPyramid = [
+  ["Zooplankton", "Krill", "Insects", "Snails"], // Tier 1 (33-512)
+  ["Small Fish", "Frogs", "Mice"], // Tier 2 (5-32)
+  ["Snakes", "Large Fish"], // Tier 3 (2,3,4)
+  ["Eagle"], // Tier 4 (1) Apex
+];
+
+console.log(randomNumber);
+`Starter money
+Click button to roll
+If has animal -- Passive money
+If no animal (getting a 0) -- No money income D:
+Create a reset/give up button
+`;
+function getRandomNumber() {
+  let money = 200;
+  let randomNumber = Math.floor(Math.random() * 1024);
+
+  while (money >= 200) {}
+}
