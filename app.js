3124c5d65d9e06526c0319daeebd4571bd005f05
TalonChen <talonc@nycstudents.net>
2026-01-29 22:14:17 -0500

    urhhh

--- app.js
+++ app.js
@@ -17,64 +17,82 @@ const animalFoodPyramid =
     { name: "Eagle" }, //1
   ];
 
-let money = 200;
+const btn = document.querySelector(".btn");
+let money = 2000;
 function getRandomAnimal() {
   let randomNumber = Math.floor(Math.random() * 1100);
   let currentAnimal = "";
-
-  while (
-    currentAnimal === animalFoodPyramid[0].name ||
-    currentAnimal === animalFoodPyramid[1].name ||
-    currentAnimal === animalFoodPyramid[2].name ||
-    currentAnimal === animalFoodPyramid[3].name
-  ) {}
-  while (
-    currentAnimal === animalFoodPyramid[4].name ||
-    currentAnimal === animalFoodPyramid[5].name ||
-    currentAnimal === animalFoodPyramid[6].name
-  ) {}
-  while (
-    currentAnimal === animalFoodPyramid[7].name ||
-    currentAnimal === animalFoodPyramid[8].name
-  ) {}
-  while (currentAnimal === animalFoodPyramid[9].name) {}
+  const colorBox = document.querySelector(".rarity");
 
   if (money >= 200) {
     money -= 200;
     if (randomNumber === 1) {
       currentAnimal = animalFoodPyramid[9].name;
+      money += 50000;
     } else if (randomNumber >= 2 && randomNumber <= 3) {
       currentAnimal = animalFoodPyramid[8].name;
+      money += 14500;
     } else if (randomNumber >= 4 && randomNumber <= 6) {
       currentAnimal = animalFoodPyramid[7].name;
+      money += 14000;
     } else if (randomNumber >= 7 && randomNumber <= 12) {
       currentAnimal = animalFoodPyramid[6].name;
+      money += 13500;
     } else if (randomNumber >= 13 && randomNumber <= 22) {
       currentAnimal = animalFoodPyramid[5].name;
+      money += 13000;
     } else if (randomNumber >= 23 && randomNumber <= 32) {
       currentAnimal = animalFoodPyramid[4].name;
+      money += 190;
     } else if (randomNumber >= 33 && randomNumber <= 128) {
       currentAnimal = animalFoodPyramid[3].name;
+      money += 190;
     } else if (randomNumber >= 129 && randomNumber <= 256) {
       currentAnimal = animalFoodPyramid[2].name;
+      money += 190;
     } else if (randomNumber >= 257 && randomNumber <= 512) {
       currentAnimal = animalFoodPyramid[1].name;
+      money += 190;
     } else if (randomNumber >= 513 && randomNumber <= 1024) {
       currentAnimal = animalFoodPyramid[0].name;
+      money += 190;
     } else {
       currentAnimal = "";
+    }
+    if (
+      currentAnimal === animalFoodPyramid[0].name ||
+      currentAnimal === animalFoodPyramid[1].name ||
+      currentAnimal === animalFoodPyramid[2].name ||
+      currentAnimal === animalFoodPyramid[3].name
+    ) {
+      colorBox.style.backgroundColor = "burlywood";
+    } else if (
+      currentAnimal === animalFoodPyramid[4].name ||
+      currentAnimal === animalFoodPyramid[5].name ||
+      currentAnimal === animalFoodPyramid[6].name
+    ) {
+      colorBox.style.backgroundColor = "cyan";
+    } else if (
+      currentAnimal === animalFoodPyramid[7].name ||
+      currentAnimal === animalFoodPyramid[8].name
+    ) {
+      colorBox.style.backgroundColor = "purple";
+    } else if (currentAnimal === animalFoodPyramid[9].name) {
+      colorBox.style.backgroundColor = "gold";
     }
   } else {
     console.log("Not enough money left --- Yikes");
   }
   if (currentAnimal !== "") {
-    console.log(`You have ${currentAnimal}`);
+    console.log(`You have ${currentAnimal}.`);
   } else {
     console.log("No animal");
   }
-}
-getRandomAnimal();
+  addToInventory(currentAnimal);
 
+  console.log(`Money left: $${money}`);
+}
+btn.addEventListener("click", getRandomAnimal);
 const animalContainer = document.querySelector(".animals");
 
 function getAnimals(animals) {
@@ -90,3 +108,12 @@ function getAnimals(animals) {
 }
 
 getAnimals(animalFoodPyramid);
+
+let inventory = [];
+
+function addToInventory(newAnimal) {
+  myZoo.push(newAnimal);
+  for (let i = 0; i < myZoo.length; i++) {
+    money -= 10;
+  }
+}
