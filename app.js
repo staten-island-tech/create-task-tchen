01ceba085c121f3a650356e00309b06e47cdd905
TalonChen <talonc@nycstudents.net>
2026-02-01 14:37:16 -0500

    change

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const animalFoodPyramid =
-  //Starter money
-  //Click button to roll
-  //If has animal -- Passive money
-  //If no animal (getting a 0) -- No money income D:
-  //Create a reset/give up button
-  [
-    { name: "Zooplankton" }, // 513 -1024
-    { name: "Krill" }, // 257 -512
-    { name: "Insects" }, // 129 -256
-    { name: "Snails" }, // 33-128
-    { name: "Small Fish" }, // 23 - 32
-    { name: "Frogs" }, // 13 - 22
-    { name: "Mice" }, // 7-12
-    { name: "Snakes" }, //4,5,6
-    { name: "Large Fish" }, //2,3
-    { name: "Eagle" }, //1
-  ];
+//Starter money
+//Click button to roll
+//If has animal -- Passive money
+//If no animal (getting a 0) -- No money income D:
+//Create a reset/give up button
+
+const animalFoodPyramid = [
+  { name: "Zooplankton" },
+  { name: "Krill" },
+  { name: "Insects" },
+  { name: "Snails" },
+  { name: "Small Fish" },
+  { name: "Frogs" },
+  { name: "Mice" },
+  { name: "Snakes" },
+  { name: "Large Fish" },
+  { name: "Eagle" },
+];
 
 const btn = document.querySelector(".btn");
 let money = 2000;
 btn.addEventListener("click", getRandomAnimal);
 const animalContainer = document.querySelector(".animals");
-let inventory = [];
+
 function getRandomAnimal() {
   let randomNumber = Math.floor(Math.random() * 1100);
   let currentAnimal = "";
@@ -109,7 +109,7 @@ function getAnimals(animals) {
 }
 
 getAnimals(animalFoodPyramid);
-
+let inventory = [];
 function addToInventory(newAnimal) {
   inventory.push(newAnimal);
   for (let i = 0; i < inventory.length; i++) {
